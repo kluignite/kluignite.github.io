23d8e7c5d804d1d4dce51eae24d89c91821ce27b
Srikar Kashyap Pulipaka <srikar.kashyap@gmail.com>
2017-01-24 00:18:26 +0530

    first commit

---
+++ overlayindex.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".button a").click(function(){
+        $(".overlay").fadeToggle(200);
+       $(this).toggleClass('btn-open').toggleClass('btn-close');
+    });
+});
+$('.overlay').on('click', function(){
+    $(".overlay").fadeToggle(200);   
+    $(".button a").toggleClass('btn-open').toggleClass('btn-close');
+    open = false;
+});
